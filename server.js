abda284342d387ebb333b8b02f3dead603d2c434
Kairarara <45175730+Kairarara@users.noreply.github.com>
2019-03-01 22:13:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
+let bodyParser=require("body-parser");
+
 var cors = require('cors');
 
 var app = express();
@@ -31,6 +33,39 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+//added code
+
+let counter=0;
+let shortenedURL=[];
+
+app.route("/api/shorturl/:urlNumber").get((req,res)=>{
+  res.redirect(shortenedURL[req.params.urlNumber]);
+})
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+// http(s)://www.example.com(/more/routes)
+let regexURL=/^https?:\/\/[\w.]+(\/[\w.])*$/
+
+app.route("/api/shorturl/new").post((req,res)=>{
+  let obj={};
+  if(regexURL.test(req.body.url)){
+    obj={
+      "original_url":req.body.url,
+      "short_url":counter
+    };
+    console.log(regexURL.test(req.body.url))
+    counter++;
+    shortenedURL.push(obj.original_url);
+  } else {
+    obj={"error":"invalid URL"};
+  }
+  res.json(obj);
+})
+
+
+
+//----------
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
